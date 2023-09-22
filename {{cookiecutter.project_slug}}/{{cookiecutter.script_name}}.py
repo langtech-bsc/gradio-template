20a13073b9b2cb539377088ca46ac8879962793e
acumplid <alexandre.cumplido@bsc.es>
2023-09-22 14:38:49 +0200

    Implemented counter and refactored file

--- {{cookiecutter.project_slug}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.script_name}}.py
@@ -2,155 +2,177 @@ import gradio as gr
 from gradio.components import Textbox, Button, Slider, Checkbox
 from AinaTheme import AinaGradioTheme
 
-callback = gr.CSVLogger()
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-def submit_input(input_, max_new_tokens, repetition_penalty, top_k, top_p, do_sample, num_beams, temperature):
-    return (
-        input_, 
-        gr.update(interactive = True), 
-        gr.update(interactive = True)
-    )
+from submit_input import submit_input
+from utils import check_max_characters, clear
 
-def change_interactive(text):
-    input_state = text
-    if input_state.strip() != "":
-        return gr.update(interactive = True)
-    else:
-        return gr.update(interactive = False)
-
-def clean(): 
-    return (
-        None, 
-        None,
-        gr.Button.update(interactive=False),
-        gr.Button.update(interactive=False),
-        gr.Slider.update(value=100),
-        gr.Slider.update(value=1.2),
-        gr.Slider.update(value=50),
-        gr.Slider.update(value=0.95),
-        gr.Checkbox.update(value=True),
-        gr.Slider.update(value=4),
-        gr.Slider.update(value=0.5),
-    )
-    
-def disable_flag_button():
-    return  (
-        gr.Button.update(interactive=False), 
-        gr.Button.update(interactive=False)
-    )
-
+MAX_NEW_TOKENS = int(os.environ.get("MAX_NEW_TOKENS", default=100))
+MAX_INPUT_CHARACTERS= int(os.environ.get("MAX_INPUT_CHARACTERS", default=15))
+SHOW_MODEL_PARAMETERS_IN_UI = os.environ.get("SHOW_MODEL_PARAMETERS_IN_UI", default=True)
 
 with gr.Blocks(**AinaGradioTheme().get_kwargs()) as demo:
     with gr.Row():
+        with gr.Column(scale=0):
+            gr.Image(
+                "./assets/BSC-blue.svg", 
+                elem_id="aguila-banner", 
+                show_label=False, show_download_button = False, 
+                show_share_button = False
+            )
         with gr.Column():
+            gr.Markdown(
+                """# Model name
+                
+                ✨ **[Model explanation]()** .
+
+                🧪 **Intended use**: 
+
+                ⚠️ **Limitations**:
+
+                👀 **Learn more about Ǎguila:** .
+                
+                """
+            )
+    with gr.Row( equal_height=False):
+        with gr.Column(variant="panel"):
+            placeholder_max_characters = Textbox(
+                visible=False,
+                interactive=False,
+                value= MAX_INPUT_CHARACTERS
+            )
             input_ = Textbox(
-                lines=10, 
-                label="Input"
+                lines=11,
+                label="Input",
+                placeholder="e.g. El mercat del barri és fantàstic hi pots trobar."
             )
+            with gr.Row(variant="panel", equal_height=True):
+                gr.HTML("""<span id="countertext" style="display: flex; justify-content: start; color:#ef4444; font-weight: bold;"></span>""")
+                gr.HTML(f"""<span id="counter" style="display: flex; justify-content: end;"> <span id="inputlenght">0</span>&nbsp;/&nbsp;{MAX_INPUT_CHARACTERS}</span>""")
 
-            max_new_tokens = Slider(
-                minimum=1, 
-                maximum=200, 
-                step=1, 
-                value=100,
-                label="Max tokens"
-            )
+            with gr.Row(variant="panel"):
+                with gr.Accordion("Model parameters", open=False, visible=SHOW_MODEL_PARAMETERS_IN_UI):
+                    max_new_tokens = Slider(
+                        minimum=1,
+                        maximum=200,
+                        step=1,
+                        value=MAX_NEW_TOKENS,
+                        label="Max tokens"
+                    )
+                    repetition_penalty = Slider(
+                        minimum=0.1,
+                        maximum=10,
+                        step=0.1,
+                        value=1.2,
+                        label="Repetition penalty"
+                    )
+                    top_k = Slider(
+                        minimum=1,
+                        maximum=100,
+                        step=1,
+                        value=50,
+                        label="Top k"
+                    )
+                    top_p = Slider(
+                        minimum=0.01,
+                        maximum=0.99,
+                        value=0.95,
+                        label="Top p"
+                    )  
+                    do_sample = Checkbox(
+                        value=True, 
+                        label="Do sample"
+                    )
+                    num_beams = Slider(
+                        minimum=1, 
+                        maximum=8, 
+                        step=1,
+                        value=4,
+                        label="Beams"
+                    )
+                    temperature = Slider(
+                        minimum=0, 
+                        maximum=1,
+                        value=0.5,
+                        label="Temperature"
+                    )
 
-            repetition_penalty = Slider(
-                minimum=0.1, 
-                maximum=10, 
-                step=0.1, 
-                value=1.2, 
-                label="Repetition penalty"
-            )
+                    parameters_compontents = [max_new_tokens, repetition_penalty, top_k, top_p, do_sample, num_beams, temperature]
 
-            top_k = Slider(
-                minimum=0, 
-                maximum=100, 
-                step=1, 
-                value=50, 
-                label="Top k"
-            )
-
-            top_p = Slider(
-                minimum=0, 
-                maximum=1, 
-                value=0.95, 
-                label="Top p"
-            )  
-
-            do_sample = Checkbox(
-                value=True, 
-                label="Do sample"
-            )
-
-            num_beams = Slider(
-                minimum=1, 
-                maximum=8, 
-                step=1, 
-                value=4, 
-                label="Beams"
-            )
-            
-            temperature = Slider(
-                minimum=0, 
-                maximum=1, 
-                value=0.5, 
-                label="Temperature"
-            )
-            
-        with gr.Column():
+        with gr.Column(variant="panel"):
             output = Textbox(
-                lines=10, 
-                label="Output", 
-                interactive=False, 
+                lines=11,
+                label="Output",
+                interactive=False,
                 show_copy_button=True
             )
+            with gr.Row(variant="panel"):
+                clear_btn = Button(
+                    "Clear", 
+                )
+                submit_btn = Button(
+                    "Submit", 
+                    variant="primary",
+                )
 
-            with gr.Row(equal_height=True):
-                btnPositive = gr.Button("👍",interactive = False)
+    with gr.Row():
+        with gr.Column():
+            gr.Examples(
+                label="Short prompts:",
+                examples=[
+                    ["""Prompt example"""],
+                ],
+                inputs=[input_] + parameters_compontents,
+                outputs=output,
+                fn=submit_input,
+            )
 
-                btnNegative= gr.Button("👎", interactive = False)
+            gr.Examples(
+                label="Zero-shot prompts",
+                examples=[
+                    ["Prompt example"],
+                ],
+                inputs=[input_] + parameters_compontents,
+                outputs=output,
+                fn=submit_input,
+            )
+            gr.Examples(
+                label="Few-Shot prompts:",
+                examples=[
+                    ["""Prompt example"""],
+                ],
+                inputs=[input_] + parameters_compontents,
+                outputs=output,
+                fn=submit_input,
+            )
 
-            with gr.Row():
-                clear_btn = Button("Clean", interactive=False)
-
-                submit_btn = Button("Submit",  variant="primary", interactive=False)
-
-    callback.setup([input_, output], "flagged_data_points")
-
-    for button in [submit_btn, clear_btn]:
-        input_.change(fn=change_interactive, inputs=[input_], outputs=button)
-
-    for button in [btnPositive, btnNegative]:
-        button.click(fn=disable_flag_button, outputs=[btnPositive, btnNegative])
-
-    btnPositive.click(
-        lambda *args: callback.flag(args, flag_option="positive"),
-        [input_, output], 
-        None, 
-        preprocess=False
+    input_.change(
+        fn=check_max_characters, 
+        inputs=[input_, placeholder_max_characters],
+        outputs=[clear_btn, submit_btn]
     )
-    
-    btnNegative.click(
-        lambda *args: callback.flag(args, flag_option="negative"),
-        [input_, output], 
-        None, 
-        preprocess=False
-    )
+
+    input_.change(fn=None, inputs=[input_, placeholder_max_characters], _js="""(i, m) => {
+        document.getElementById('countertext').textContent =  i.length > m && 'Max length ' + m + ' characters. ' || ''
+        document.getElementById('inputlenght').textContent = i.length + '  '
+        document.getElementById('inputlenght').style.color =  (i.length > m) ? "#ef4444" : "";
+    }""")
 
     clear_btn.click(
-        fn=clean, 
+        fn=clear, 
         inputs=[], 
-        outputs=[input_, output, btnPositive, btnNegative, max_new_tokens, repetition_penalty, top_k, top_p, do_sample, num_beams, temperature], 
+        outputs=[input_, output] + parameters_compontents, 
         queue=False
     )
-
+    
     submit_btn.click(
         fn=submit_input, 
-        inputs=[input_, max_new_tokens, repetition_penalty, top_k, top_p, do_sample, num_beams, temperature], 
-        outputs=[output, btnPositive, btnNegative]
+        inputs=[input_] + parameters_compontents, 
+        outputs=[output]
     )
-    
-demo.queue(concurrency_count=1, api_open=False)
-demo.launch(show_api=False)
+
+if __name__ == "__main__":
+    demo.queue(concurrency_count=1, api_open=False)
+    demo.launch(show_api=False)
